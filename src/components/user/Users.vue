16d25d1bd93a4b9319056ee4fca5609bf9409706
jass <972743696@qq.com>
2021-07-20 11:16:31 +0800

    完善细节

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -112,6 +112,23 @@
           </span>
         </el-dialog>
 
+      <!-- 分配权限的对话框 -->
+        <el-dialog title="分配权限" @close="setRoleDialogClose" :visible.sync="setDialogRoleVisible" width="40%">
+          <div>
+            <p>当前的用户：{{ userInfo.username }}</p>
+            <p>当前的角色：{{ userInfo.role_name }}</p>
+            <p>
+              分配新角色:
+              <el-select v-model="selectedRoleId" placeholder="请选择">
+                <el-option v-for="item in rolesList" :key="item.id" :label="item.roleName" :value="item.id"></el-option>
+              </el-select>
+            </p>
+          </div>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="setDialogRoleVisible = false">取 消</el-button>
+            <el-button type="primary" @click="saveRoleInfo">确 定</el-button>
+          </span>
+        </el-dialog>
     </el-card>
     </div>
 </template>
@@ -178,7 +195,15 @@ export default {
           { required: true, message: '请输入手机号', trigger: 'blur' },
           { validator: checkMobile, trigger: 'blur' }
         ]
-      }
+      },
+      // 控制分配角色对话框的显示与隐藏
+      setDialogRoleVisible: false,
+      // 需要被分配角色的用户信息
+      userInfo: {},
+      // 所有角色的数据列表
+      rolesList: {},
+      // 已选中的角色ID值
+      selectedRoleId: ''
     }
   },
 
@@ -250,13 +275,13 @@ export default {
           message: '添加用户成功！',
           duration: 1500
         })
-        // this.$notify({
-        //   type: 'success',
-        //   title: '添加成功',
-        //   message: '注意当您添加成功后：<br/>1.需要开启该用户 <br/>2.请为该用户分配角色(默认角色无效)',
-        //   dangerouslyUseHTMLString: true,
-        //   duration: 0
-        // })
+        this.$notify({
+          type: 'success',
+          title: '添加成功',
+          message: '注意当您添加成功后：<br/>1.需要开启该用户 <br/>2.请为该用户分配角色(默认角色无效)',
+          dangerouslyUseHTMLString: true,
+          duration: 0
+        })
         // 隐藏 dialog对话框
         this.addDialogVisible = false
         // 重新获取列表数据
@@ -345,6 +370,53 @@ export default {
       })
       // 更新数据列表
       this.getUserList()
+    },
+    // 展示分配角色的对话框
+    async setRole (userInfo) {
+      // 展示对话框之前获取所有角色列表
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) {
+        return this.$message.error({
+          message: res.meta.msg,
+          duration: 1500
+        })
+      }
+      this.rolesList = res.data
+
+      this.setDialogRoleVisible = true
+      this.userInfo = userInfo
+    },
+    // 点击按钮 分配角色
+    async saveRoleInfo () {
+      if (!this.selectedRoleId === true) {
+        return this.$message.info({
+          message: '请选则要分配的角色',
+          duration: 1500
+        })
+      }
+
+      const { data: res } = await this.$http.put(`users/${this.userInfo.id}/role`, {
+        rid: this.selectedRoleId
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error({
+          message: res.meta.msg,
+          duration: 1500
+        })
+      }
+      this.$message.success({
+        message: '更新角色成功!',
+        duration: 1500
+      })
+      this.getUserList()
+      this.setDialogRoleVisible = false
+    },
+    // 监听分配角色对话框关闭
+    setRoleDialogClose () {
+      // 相当于清空当前选中项
+      this.selectedRoleId = ''
+      // 清空当前分配角色的数据列表
+      this.userInfo = {}
     }
   }
 
